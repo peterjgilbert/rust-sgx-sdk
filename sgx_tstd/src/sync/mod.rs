3d5371cdc782d94d44898e32661bc864be6b8f83
Nick Hynes <nhynes@berkeley.edu>
2018-02-23 09:52:58 -0800

    sync::SgxX -> sync::X

--- sgx_tstd/src/sync/mod.rs
+++ sgx_tstd/src/sync/mod.rs
@@ -43,7 +43,12 @@ pub use core::sync::atomic;
 
 pub use self::barrier::{Barrier, BarrierWaitResult};
 pub use self::condvar::{SgxCondvar, SgxThreadCondvar};
+pub use self::condvar::SgxCondvar as Condvar;
+pub use self::condvar::SgxThreadCondvar as ThreadCondvar;
 pub use self::mutex::{SgxMutex, SgxMutexGuard, SgxThreadMutex};
+pub use self::mutex::SgxMutex as Mutex;
+pub use self::mutex::SgxThreadMutex as ThreadMutex;
+pub use self::mutex::SgxMutexGuard as MutexGuard;
 pub use self::remutex::{SgxReentrantMutex, SgxReentrantMutexGuard, SgxReentrantThreadMutex};
 pub use self::once::{Once, OnceState, ONCE_INIT};
 pub use self::rwlock::{SgxRwLock, SgxRwLockReadGuard, SgxRwLockWriteGuard, SgxThreadRwLock};
